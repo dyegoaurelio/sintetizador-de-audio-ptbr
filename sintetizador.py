af55ccd50164146573666129c94afc052407faa6
Dyego Aurélio <dyegoaurelio@gmail.com>
2020-10-09 13:55:56 -0300

    first commit

---
+++ sintetizador.py
@@ -0,0 +1,11 @@
+import pyttsx3
+
+speaker = pyttsx3.init()
+speaker.setProperty('voice', speaker.getProperty('voices')[54].id) 
+speaker.setProperty('rate', 150)
+
+texto = open('texto.txt','r').read()
+
+speaker.save_to_file(texto, 'output.mp3')
+speaker.say(texto)
+speaker.runAndWait()
